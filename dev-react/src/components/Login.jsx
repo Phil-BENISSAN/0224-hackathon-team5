4b6a17fe8a47729120c577a2617c928f0fa45246
KevinDavoust <kevin.davoust@orange.fr>
2024-07-03 19:52:19 +0200

    login form

--- dev-react/src/components/Login.jsx
+++ dev-react/src/components/Login.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
 import "../styles/forms.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = { email, password };
+  };
+
   return (
     <main>
       <section className="form">
-        <div>Login</div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="password">Mot de passe</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" onClick={handleSubmit}>
+            S'inscrire
+          </button>
+        </form>
       </section>
       <section></section>
     </main>
