decade0c3d5c337c0c02e95bb513631876f63287
sorrento974 <sorrento974@gmail.com>
2024-07-03 15:16:15 +0200

    modification docker

--- dev-react/src/App.jsx
+++ dev-react/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './index.css'
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
--- dev-react/src/components/Header.jsx
+++ dev-react/src/components/Header.jsx
@@ -7,8 +7,12 @@ export default function Header() {
     <header>
      <Link
      to="/actus">
-      Fil d'actus
+      Beeskip
      </Link>
+     <Link><h2 className='features1'>Features</h2></Link>
+     <Link><h2 className='features2'>Features</h2></Link>
+     <Link><h2 className='connexion'>Connexion</h2></Link>
+     <Link><h2 className='try'>Essayer gratuitement</h2></Link>
     </header>
   );
 }
