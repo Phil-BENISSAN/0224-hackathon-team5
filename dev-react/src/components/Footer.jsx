4700f6f61c1cad157a7bd2f54b55163a55a755a8
sachaig <sacha.isambourg01@gmail.com>
2024-07-03 10:42:58 +0200

    React router dom

---
+++ dev-react/src/components/Footer.jsx
@@ -0,0 +1,6 @@
+export default function Footer() {
+
+    return <footer>
+        <h1>hi</h1>
+        </footer>
+}
--- dev-react/src/main.jsx
+++ dev-react/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import Home from "./pages/HomePage";
+import Contact from "./pages/HomePage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
