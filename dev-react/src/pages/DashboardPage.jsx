98e3a6ea07cb22a53472736d0d99d31be1f7f8a5
sachaig <sacha.isambourg01@gmail.com>
2024-07-04 15:35:20 +0200

    add

--- dev-react/src/pages/DashboardPage.jsx
+++ dev-react/src/pages/DashboardPage.jsx
@@ -1,16 +1,58 @@
 import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "../styles/sidebar.css";
+import logoSrc from "../assets/Biskeep.png";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineTeam } from "react-icons/ai";
+import { IoStatsChartSharp } from "react-icons/io5";
+import { IoIosCall } from "react-icons/io";
+import { IoMdCalendar } from "react-icons/io";
 
 function Dashboard() {
   return (
-    <>
-      <div>
-        <Link className="logo" to="/actus">
-          Biskeep
+    <div className="sidebar">
+      <div className="sidebar-logo">
+        <img src={logoSrc} alt="Biskeep Logo" />
+      </div>
+      <nav className="sidebar-nav">
+        <ul>
+          <li>
+            <Link to="/" className="sidebar-link">
+              <AiOutlineHome className="icons" />
+              Accueil
+            </Link>
+          </li>
+          <li>
+            <Link to="/equipes" className="sidebar-link">
+              <AiOutlineTeam className="icons" /> Equipes
+            </Link>
+          </li>
+          <li>
+            <Link to="/statistiques" className="sidebar-link">
+              <IoStatsChartSharp className="icons" /> Statistiques
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/evenements" className="sidebar-link">
+              <IoMdCalendar className="icons" /> Events
+            </Link>
+          </li>
+          <li>
+            <Link to="/statistiques" className="sidebar-link">
+              <IoIosCall className="icons" /> A propos
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="sidebar-footer">
+        <Link to="/logout" className="sidebar-link">
+          <p></p>Se déconnecter
         </Link>
       </div>
-      <Outlet />;
-    </>
+    </div>
   );
+  <Outlet />;
 }
 
 export default Dashboard;
