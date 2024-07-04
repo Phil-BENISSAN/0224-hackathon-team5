1844461b820313ca83c88794f0148d74822a7af1
Delgorithm <delgorithm@gmail.com>
2024-07-04 14:43:25 +0200

    Fix : Adrien

--- dev-react/src/main.jsx
+++ dev-react/src/main.jsx
@@ -5,64 +5,64 @@ import App from "./App.jsx";
 import "./index.css";
 import Home from "./pages/HomePage";
 import Contact from "./pages/HomePage";
-import News from "./components/NewsPage.jsx";
+import News from "./pages/NewsPage.jsx";
 import AuthPage from "./pages/AuthPage.jsx";
 import Register from "./components/Register.jsx";
 import Login from "./components/Login.jsx";
 import Dashboard from "./pages/DashboardPage.jsx";
-import GamesRank from './components/GamesRank';
-import TeamsRank from './components/TeamsRank';
+import GamesRank from "./components/GamesRank";
+import TeamsRank from "./components/TeamsRank";
 
 const router = createBrowserRouter([
-  {
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      {
-        path: "/auth",
-        element: <AuthPage />,
-        children: [
-          {
-            path: "register",
-            element: <Register />,
-          },
-          {
-            path: "login",
-            element: <Login />,
-          },
-        ],
-      },
-      {
-        path: "/dashboard",
-        element: <Dashboard />,
-        children: [
-          {
-            path: "actus",
-            element: <News />,
-          },
-          {
-            path: "gamesrank",
-            element: <GamesRank />,
-          },
-          {
-            path: "teamsrank",
-            element: <TeamsRank />,
-          },
-        ],
-      },
-    ],
-  },
+	{
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/contact",
+				element: <Contact />,
+			},
+			{
+				path: "/auth",
+				element: <AuthPage />,
+				children: [
+					{
+						path: "register",
+						element: <Register />,
+					},
+					{
+						path: "login",
+						element: <Login />,
+					},
+				],
+			},
+			{
+				path: "/dashboard",
+				element: <Dashboard />,
+				children: [
+					{
+						path: "actus",
+						element: <News />,
+					},
+					{
+						path: "gamesrank",
+						element: <GamesRank />,
+					},
+					{
+						path: "teamsrank",
+						element: <TeamsRank />,
+					},
+				],
+			},
+		],
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
--- dev-react/src/pages/DashboardPage.jsx
+++ dev-react/src/pages/DashboardPage.jsx
@@ -1,16 +1,17 @@
 import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Dashboard() {
-  return (
-    <>
-      <div>
-        <Link className="logo" to="/actus">
-          Biskeep
-        </Link>
-      </div>
-      <Outlet />;
-    </>
-  );
+	return (
+		<>
+			<div>
+				<Link className="logo" to="/">
+					Biskeep
+				</Link>
+			</div>
+			<Outlet />;
+		</>
+	);
 }
 
 export default Dashboard;
