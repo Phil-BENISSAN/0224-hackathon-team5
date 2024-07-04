5a7ca213b70d3ede0917bd8625b808ff02215c06
Delgorithm <delgorithm@gmail.com>
2024-07-04 15:31:41 +0200

    Fix : Adrien

--- dev-react/src/components/TeamsRank.jsx
+++ dev-react/src/components/TeamsRank.jsx
@@ -1,9 +1,29 @@
-export default function TeamsRank() {
-    return (
-      <main>  
-        <h1>hi</h1>
+import teamsData from "../data/teamsRank.json";
 
-      </main>
-    );
-  }
-  
+export default function TeamsRank() {
+	console.log(teamsData);
+	return (
+		<>
+			{/* Début : Dashboard : Barre de gauche */}
+			<h1>hi</h1>
+			{/* Fin : Dashboard : Barre de gauche */}
+
+			{/* Début élément central de la page */}
+			<h2>Top équipes inclusives</h2>
+			<div className="team">
+				{teamsData.map((team, index) => (
+					<div key={index}>
+						<p>{team.title}</p>
+						<div>
+							<p>Indice d'inclusivité : {team.inclusivity_index}</p>
+							<p>Popularité : {team.popularity}</p>
+							<p>Nationalité : {team.nationality}</p>
+						</div>
+					</div>
+				))}
+			</div>
+
+			{/* Fin élément central de la page */}
+		</>
+	);
+}
