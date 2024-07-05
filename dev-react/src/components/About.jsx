f0ace7100d5f9e6ea233969678397bf880ae8259
Delgorithm <delgorithm@gmail.com>
2024-07-05 04:01:14 +0200

    les caméras

--- dev-react/src/components/About.jsx
+++ dev-react/src/components/About.jsx
@@ -1,7 +1,15 @@
+import "../styles/about.css";
+import inclusivityData from "../data/aboutData.json";
+
 function About() {
 	return (
-		<div className="big-container">
-			<p>About</p>
+		<div className="big-container-about">
+			{inclusivityData.map((chapter, index) => (
+				<div key={index} className="chapter">
+					<h2>{chapter.title}</h2>
+					<p>{chapter.content}</p>
+				</div>
+			))}
 		</div>
 	);
 }
